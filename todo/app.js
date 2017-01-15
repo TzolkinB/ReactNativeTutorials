156ea91ea52dbc7a7bc3a1270b581c5bb13a79d6
Kimberly Bell <kimberly.bell@tma1.com>
2017-01-15 16:32:18 -0500

    completed video 5 of egghead todo app

---
+++ todo/app.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import Header from './header';
+import Footer from './footer';
+import Row    from './row';
+import {
+  View, Text, StyleSheet,
+  Platform, ListView, Keyboard
+} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+
+    this.state = {
+      value: "",
+      items: [],
+      allComplete: false,
+      dataSource: ds.cloneWithRows([])
+    }
+    setSource = this.setSource.bind(this);
+    handleAddItem = this.handleAddItem.bind(this);
+    handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+  }
+
+  setSource(items, itemsDataSource, otherState ={}) {
+    this.setState({
+      items,
+      dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
+      ...otherState
+    })
+  }
+
+  handleAddItem() {
+    if(!this.state.value){
+      return;
+    }
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ]
+    setSource(newItems, newItems, { value: ''})
+  }
+
+  handleToggleAllComplete() {
+   const complete = !this.state.allComplete;
+   const newItems = this.state.items.map((item) => ({
+    ...item,
+    complete
+   }))
+   setSource(newItems, newItems, { allComplete: complete})
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Header
+          value={this.state.value}
+          onAddItem={handleAddItem}
+          onChange={(value) => this.setState({value})}
+          onToggleAllComplete={handleToggleAllComplete}
+        />
+        <View style={styles.content}>
+          <ListView
+            style={styles.list}
+            enableEmptySections
+            dataSource={this.state.dataSource}
+            onScroll={() => Keyboard.dismiss()}
+            renderRow={({ key, ...value}) => {
+              return (
+                <Row
+                  key={key}
+                  {...value}
+                />
+              )
+            }}
+            renderSeparator={(sectionId, rowId) => {
+              return <View key={rowId} style={styles.separator}/>
+            }}
+        />
+        </View>
+        <Footer />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    ...Platform.select({
+      ios: { paddingTop: 30 }
+    })
+  },
+  content: {
+    flex: 1
+  },
+  list: {
+    backgroundColor: '#FFF'
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: '#F5F5F5'
+  }
+})
+
+export default App;
