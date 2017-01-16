d14cd9698a2d25c1f3f89ceb1c2abd4752096c50
Kimberly Bell <kimberly.bell@tma1.com>
2017-01-16 12:07:32 -0500

    added react-native-vector-icons to use fontawesome

--- todo/app.js
+++ todo/app.js
@@ -4,8 +4,17 @@ import Footer from './footer';
 import Row    from './row';
 import {
   View, Text, StyleSheet,
-  Platform, ListView, Keyboard
+  Platform, ListView, Keyboard,
+  AsyncStorage, ActivityIndicator
 } from 'react-native';
+
+const filterItems = (filter, items) => {
+  return items.filter((item) => {
+    if(filter === "ALL") return true;
+    if(filter === "COMPLETED") return item.complete;
+    if(filter === "ACTIVE") return !item.complete;
+  })
+}
 
 class App extends Component {
   constructor(props) {
@@ -13,9 +22,11 @@ class App extends Component {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
 
     this.state = {
+      loading: true,
       value: "",
       items: [],
       allComplete: false,
+      filter: "ALL",
       dataSource: ds.cloneWithRows([])
     }
     setSource = this.setSource.bind(this);
@@ -23,6 +34,23 @@ class App extends Component {
     handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
     handleToggleComplete = this.handleToggleComplete.bind(this);
     handleRemoveItem = this.handleRemoveItem.bind(this);
+    handleFilter = this.handleFilter.bind(this);
+    handleClearComplete = this.handleClearComplete.bind(this);
+    handleUpdateText = this.handleUpdateText.bind(this);
+    handleToggleEditing = this.handleToggleEditing.bind(this);
+  }
+
+  componentWillMount() {
+    AsyncStorage.getItem("items").then((json) => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items, {loading: false});
+      } catch(error) {
+        this.setState({
+          loading: false
+        })
+      }
+    })
   }
 
   setSource(items, itemsDataSource, otherState ={}) {
@@ -31,6 +59,16 @@ class App extends Component {
       dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
       ...otherState
     })
+    AsyncStorage.setItem("items", JSON.stringify(items));
+  }
+
+  handleFilter(filter) {
+    this.setSource(this.state.items, filterItems(filter, this.state.items), {filter})
+  }
+
+  handleClearComplete() {
+    const newItems = filterItems("ACTIVE", this.state.items);
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
 
   handleAddItem() {
@@ -45,7 +83,7 @@ class App extends Component {
         complete: false
       }
     ]
-    setSource(newItems, newItems, { value: ''})
+    setSource(newItems, filterItems(this.state.filter, newItems), { value: ''})
   }
 
   handleToggleAllComplete() {
@@ -54,7 +92,7 @@ class App extends Component {
     ...item,
     complete
    }))
-   setSource(newItems, newItems, { allComplete: complete})
+   setSource(newItems, filterItems(this.state.filter, newItems), { allComplete: complete})
   }
 
   handleToggleComplete(key, complete) {
@@ -67,14 +105,36 @@ class App extends Component {
         complete
       }
     })
-    this.setSource(newItems, newItems);
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
 
   handleRemoveItem(key) {
     const newItems = this.state.items.filter((item) => {
       return item.key != key
     })
-    this.setSource(newItems, newItems);
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+
+  handleUpdateText(key, text) {
+    const newItems = this.state.items.map((item) => {
+      if(item.key != key) return item;
+      return {
+        ...item,
+        text
+      }
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+
+  handleToggleEditing(key, editing) {
+    const newItems = this.state.items.map((item) => {
+      if(item.key != key) return item;
+      return {
+        ...item,
+        editing
+      }
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
 
   render() {
@@ -96,6 +156,8 @@ class App extends Component {
               return (
                 <Row
                   key={key}
+                  onUpdate={(text) => handleUpdateText(key, text)}
+                  onToggleEdit={(editing) => handleToggleEditing(key, editing)}
                   onRemove={() => handleRemoveItem(key)}
                   onComplete={(complete) => handleToggleComplete(key, complete)}
                   {...value}
@@ -107,7 +169,20 @@ class App extends Component {
             }}
         />
         </View>
-        <Footer />
+        <Footer
+          onFilter={handleFilter}
+          filter={this.state.filter}
+          count={filterItems("ACTIVE", this.state.items).length}
+          onClearComplete={handleClearComplete}
+        />
+        {this.state.loading &&
+          <View style={styles.loading}>
+            <ActivityIndicator
+              animating
+              size="large"
+            />
+          </View>
+        }
       </View>
     );
   }
@@ -120,6 +195,16 @@ const styles = StyleSheet.create({
     ...Platform.select({
       ios: { paddingTop: 30 }
     })
+  },
+  loading: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)"
   },
   content: {
     flex: 1
--- todo/row.js
+++ todo/row.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {
   View, Text, StyleSheet,
-  Switch, TouchableOpacity
+  Switch, TouchableOpacity, TextInput
 } from 'react-native';
 
 class Row extends Component {
   render() {
+
     const {complete} = this.props;
 
+    const textView = (
+      <TouchableOpacity 
+        style={styles.textWrap}
+        onLongPress={() => this.props.onToggleEdit(true)}
+      >
+        <Text style={[styles.text, complete && styles.complete]}>
+          {this.props.text}
+        </Text>
+      </TouchableOpacity>
+    )
+
+    const removeButton = (
+      <TouchableOpacity onPress={this.props.onRemove}>
+        <FontAwesome name='trash' />
+      </TouchableOpacity>
+    )
+
+    const textEdit = (
+      <View style={styles.textWrap}>
+        <TextInput
+          onChangeText={this.props.onUpdate}
+          autoFocus
+          value={this.props.text}
+          style={styles.input}
+          multiline
+        />
+      </View>
+    )
+
+    const saveButton = (
+      <TouchableOpacity 
+        style={styles.done}
+        onPress={() => this.props.onToggleEdit(false)}>
+        <Text style={styles.doneText}>Save</Text>
+      </TouchableOpacity> 
+    )
+    
     return(
       <View style={styles.container}>
         <Switch
           value={complete}
           onValueChange={this.props.onComplete}
         />
-        <View style={styles.textWrap}>
-          <Text style={[styles.text, complete && styles.complete]}>
-            {this.props.text}
-          </Text>
-          <TouchableOpacity onPress={this.props.onRemove}>
-            <Text style={styles.remove}>X</Text>
-          </TouchableOpacity>
-        </View>
+        {this.props.editing ? textEdit : textView}
+        {this.props.editing ? saveButton : removeButton}
       </View>
     );
   }
@@ -32,18 +65,35 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: "row",
     alignItems: "flex-start",
-    justifyContent: "space-between"
+    justifyContent: "flex-start"
+  },
+  input: {
+    height: 100,
+    flex: 1,
+    fontSize: 24,
+    padding: 0,
+    color: "#4d4d4d"
   },
   textWrap: {
     flex: 1,
-    marginHorizontal: 5,
+    marginHorizontal: 10,
+  },
+  done: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#7be290",
+    padding: 7
+  },
+  doneText: {
+    color: "#4d4d4d",
+    fontSize: 20
   },
   complete: {
     textDecorationLine: "line-through"
   },
   text: {
     fontSize: 20,
-    color: "#4d4d4d"
+    color: "#4d4d4d",
   },
   remove: {
     fontSize: 20,
